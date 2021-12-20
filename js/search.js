64ad570558d62f2ff49a35bbe37fa9c83d53bb92
robertduplock <robertduplock@users.noreply.github.com>
2021-12-20 03:55:18 +0000

    deploy: 837c3790d75909d2d2192da2e7104aaa9df6536a

--- js/search.js
+++ js/search.js
@@ -3,7 +3,7 @@
  */
 (function() {
 
-  var app = {};
+  let app = {};
 
   /**
    * Index search results.
@@ -14,16 +14,18 @@
     fetch('/search/index.json', {
       method: 'get',
     })
-    .then((res) => res.json())
-    .then((res) => {
+    .then((response) => response.json())
+    .then((response) => {
 
       // Init the lunr search index.
       const idx = lunr(function () {
         this.ref('id')
         this.field('title')
         this.field('content')
+        this.field('tags')
+        this.metadataWhitelist = ['position']
 
-        res.forEach(function (doc) {
+        response.forEach(function (doc) {
           this.add(doc)
         }, this)
       })
@@ -52,16 +54,36 @@
       // Show the results.
       document.getElementById('results-summary').classList.remove('dn');
       document.getElementById('results-summary-count').innerText = (results.length);
-      results.map(function (result) {
-        const resultElement = document.getElementById(result.ref);
-        container.appendChild(resultElement);
-        resultElement.classList.remove('dn');
+      results.map((result) => {
+        response.map((responseItem) => {
+          if (result.ref === responseItem.id) {
+            container.insertAdjacentHTML('beforeend', app.searchTemplate(responseItem));
+          }
+        })
       });
 
     })
     .catch((err) => {
       console.log({ err });
     });
+  }
+
+  /**
+   * Renders a search result teaser.
+   *
+   * @param data
+   *   An object of data for the element.
+   */
+  app.searchTemplate = (data) => {
+    return `
+      <div class="pa2 pa3-ns bb b--black-20" id="${data.id}">
+        <a class="link dark-gray flex" href="${data.id}">
+          <span class="b f4">${data.title}</span>
+          <span class="pt1 ml2">${data.section.charAt(0).toUpperCase() + data.section.slice(1)}</span>
+        </a>
+        <p>${data.summary}</p>
+      </div>
+    `;
   }
 
   // Init the search.
